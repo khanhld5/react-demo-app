8215e9f43247e1d6b7f88c5c0fd7c8da1754274e
lekhanh <khanhld@vmodev.com>
2021-04-19 10:49:18 +0700

    [LDK] integrate immerJS on todoList

--- src/state/reducers/todoList/todoListReducer.js
+++ src/state/reducers/todoList/todoListReducer.js
@@ -1,5 +1,6 @@
 import * as act from "../../../Constant/todo";
 import * as filter from "../../../Constant/filter";
+import produce from "immer";
 
 const initailState = {
   list: JSON.parse(localStorage.getItem("list")) || [],
@@ -7,121 +8,102 @@ const initailState = {
   filter: filter.ALL,
 };
 
-const todoListReducer = (state = initailState, action) => {
+const todoListReducer = produce((draft, action) => {
   const payload = action.payload;
   const checkAllDone = (list) => {
     let allDone = false;
-    if (list.every((item) => item.done === true)) return (allDone = true);
+    if (list.length && list.every((item) => item.done === true))
+      return (allDone = true);
     return allDone;
   };
 
   switch (action.type) {
-    // case act.INITIAL_TODO_LIST: {
-    //   const list = payload.list;
-    //   return { ...state, list };
-    // }
+    case act.ADD_TODO:
+      {
+        let id = 0;
+        const title = payload.title;
+        const list = draft.list;
+        if (list.length) id = list[list.length - 1].id + 1;
+        const todo = { id: id, done: false, title };
+        list.push(todo);
+        draft.allDone = false;
+      }
+      break;
 
-    case act.ADD_TODO: {
-      let id = 0;
-      const title = payload.title;
-      const stateList = state.list;
-      const allDone = false;
-      if (stateList.length) id = stateList[stateList.length - 1].id + 1;
-      const todo = { id: id, done: false, title };
-      return { ...state, list: [...stateList, todo], allDone };
-    }
+    case act.TODO_DONE:
+      {
+        const id = payload.id;
+        const list = draft.list;
+        for (let todo of list) {
+          if (todo.id === id) {
+            todo.done = !todo.done;
+            break;
+          }
+        }
+        draft.allDone = checkAllDone(list);
+      }
+      break;
 
-    case act.TODO_DONE: {
-      // const todo = payload.todo;
-      const id = payload.id;
-      const stateList = state.list;
-      const newList = [...stateList];
-      // need test
-      for (let todo of newList) {
-        if (todo.id === id) {
-          todo.done = !todo.done;
-          break;
+    case act.TODO_ALLDONE:
+      {
+        const list = draft.list;
+        const allDone = draft.allDone;
+        if (allDone) {
+          for (let i = 0; i < list.length; i++) {
+            list[i].done = false;
+          }
+          draft.allDone = false;
+        } else {
+          for (let i = 0; i < list.length; i++) {
+            list[i].done = true;
+          }
+          draft.allDone = true;
         }
       }
-      const allDone = checkAllDone(stateList);
-      return { ...state, list: newList, allDone };
-    }
+      break;
 
-    case act.TODO_ALLDONE: {
-      const stateList = state.list;
-      const newList = [...stateList];
-      const stateAllDone = state.allDone;
-      if (stateAllDone) {
-        for (let i = 0; i < newList.length; i++) {
-          newList[i].done = false;
-        }
-        const allDone = false;
-        return { ...state, list: newList, allDone };
-      } else {
-        for (let i = 0; i < stateList.length; i++) {
-          newList[i].done = true;
-        }
-        const allDone = true;
-        return { ...state, list: newList, allDone };
-      }
-    }
-
-    case act.EDIT_TODO: {
-      const todo = payload.todo;
-      const id = todo.id;
-      const title = todo.title;
-      const stateList = state.list;
-      const newList = [...stateList];
-
-      for (let todo of newList) {
-        if (todo.id === id) {
-          todo.title = title;
-          break;
+    case act.EDIT_TODO:
+      {
+        const todo = payload.todo;
+        const id = todo.id;
+        const title = todo.title;
+        const list = draft.list;
+        for (let todo of list) {
+          if (todo.id === id) {
+            todo.title = title;
+            break;
+          }
         }
       }
-      return { ...state, list: newList };
-    }
+      break;
 
-    case act.REMOVE_TODO: {
-      const id = payload.id;
-      const stateList = state.list;
-      const newList = stateList.filter((todo) => todo.id !== id);
-      const allDone = checkAllDone(newList);
-      return { ...state, list: newList, allDone };
-    }
+    case act.REMOVE_TODO:
+      {
+        const id = payload.id;
+        const newList = draft.list.filter((todo) => todo.id !== id);
+        draft.allDone = checkAllDone(newList);
+        draft.list = newList;
+      }
+      break;
 
-    case act.REMOVE_COMPLETE_TODO: {
-      const stateList = state.list;
-      const newList = stateList.filter((todo) => todo.done === false);
-      return { ...state, list: newList, allDone: false };
-    }
+    case act.REMOVE_COMPLETE_TODO:
+      {
+        const newlist = draft.list.filter((todo) => todo.done === false);
+        draft.allDone = false;
+        draft.list = newlist;
+      }
+      break;
 
-    // case act.INITIAL_ALLDONE: {
-    //   const allDone = payload.allDone;
-    //   return { ...state, allDone };
-    // }
-
-    // might not need?
-    // case act.ALLDONE_TRUE: {
-    //   return { allDone: true };
-    // }
-
-    // case act.ALLDONE_FALSE: {
-    //   return { allDone: false };
-    // }
-
-    case act.FILTER_ALL: {
-      return { ...state, filter: filter.ALL };
-    }
-    case act.FILTER_ACTIVE: {
-      return { ...state, filter: filter.ACTIVE };
-    }
-    case act.FILTER_COMPLETE: {
-      return { ...state, filter: filter.COMPLETE };
-    }
-
+    case act.FILTER_ALL:
+      draft.filter = filter.ALL;
+      break;
+    case act.FILTER_ACTIVE:
+      draft.filter = filter.ACTIVE;
+      break;
+    case act.FILTER_COMPLETE:
+      draft.filter = filter.COMPLETE;
+      break;
     default:
-      return state;
   }
-};
+}, initailState);
 export default todoListReducer;
